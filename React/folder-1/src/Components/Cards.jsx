1e3f58bd797413f99cbebf2644c841ee85e0d346
Rajesh Sahu <rajeshsahu3406@gmail.com>
2026-05-04 16:52:49 +0530

    learning about the new libraries and framework

--- React/folder-1/src/Components/Cards.jsx
+++ React/folder-1/src/Components/Cards.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import ElectricBorder from "./ElectricBorder.jsx";
 
 const Cards = (props) => {
 
   return (
     <>
+    <ElectricBorder
+  color="#7df9ff"
+  speed={1}
+  chaos={0.12}
+  thickness={2}
+  style={{ borderRadius: 16 }}
+>
     <div className="card">
       <img src={props.img} alt={props.user} />
       <h3>{props.user}</h3>
       <p>{props.age} years old</p>
       <button className="btn">Read More</button>
      </div>
+ </ElectricBorder>
     </>
   )
 }
