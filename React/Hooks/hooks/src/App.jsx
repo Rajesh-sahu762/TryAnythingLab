2f5fc1baba69cc2580886b0eef983a698f064806
Rajesh Sahu <rajeshsahu3406@gmail.com>
2026-05-15 23:47:52 +0530

    learning the comcept of two-way binding and form handling

--- React/Hooks/hooks/src/App.jsx
+++ React/Hooks/hooks/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import State from './Hook-understanding/useState/state'
+// import State from './Hook-understanding/useState/state'
+
 
 const App = () => {
+
+  const formHandling = (e) => {
+    e.preventDefault()
+    console.log('form submitted')
+  }
+
+
   return (
    <>
-    <State />
+
+   <form onSubmit={ (e) => {
+    formHandling(e)
+   }}>
+    <input type="text" placeholder='enter name' />
+    <button>submit</button>
+
+   </form>
+  
+    {/* <State /> */}
    </>
   )
 }
