5d6456aeaae05974ac71c83ff3e9bfe3b96c3433
Rajesh Sahu <rajeshsahu3406@gmail.com>
2026-05-19 01:01:21 +0530

    mini project done

--- React/Notes/notes/src/App.jsx
+++ React/Notes/notes/src/App.jsx
@@ -5,15 +5,36 @@ const App = () => {
 
   const [title, setTitle] = useState('')
   const [detail, setDetail] = useState('')
+  const [task, setTask] = useState([])
+  
 
 
   const formHandling = (e) => {
     e.preventDefault()
-    console.log(title,detail)
+
+    const copyTask = [...task]
+
+    copyTask.push({
+      title : title,
+      detail : detail
+    })
+
+    setTask(copyTask)
+    // console.log(task)
+
     setTitle('')
     setDetail('')
   }
 
+
+  const deleteNote =(idx) => {
+    const copyTask = [...task]
+  
+    copyTask.splice(idx,1)
+
+    setTask(copyTask)
+
+  }
 
   return (
     <>
@@ -38,13 +59,25 @@ const App = () => {
 
 
 
-    <div className="w-1/2 flex flex-wrap">
+    <div className="w-1/2 flex flex-wrap relative">
     <h1 className="p-10 font-bold text-3xl">Recent Notes</h1>
-    <div className="recent flex flex-wrap gap-10">
-    <div className="cards bg-white h-73 w-3xs rounded-2xl "></div>
-    <div className="cards bg-white h-73 w-3xs rounded-2xl "></div>
-    <div className="cards bg-white h-73 w-3xs rounded-2xl "></div>
-    <div className="cards bg-white h-73 w-3xs rounded-2xl "></div>
+    <div className="recent flex absolute top-25 left-10 justify-start flex-wrap gap-10">
+
+    {task.map(function(elem,idx){
+      return  <div key={idx} className="cards justify-between flex flex-col items-start bg-white h-73 w-3xs rounded-2xl ">
+        <div>
+      <h3 className="text-black font-bold text-left p-5 pb-0 leading-tight text-3xl">{elem.title}</h3>
+      <p className="text-gray-500 leading-tight p-6 pt-2 text-left " >{elem.detail}</p>
+      </div>
+      
+      <button className="bg-red-800 text-white text-center place-self-center cursor-pointer mb-8 px-4 p-2 rounded" onClick={()=> {
+        deleteNote(idx)
+      }}>delete Note</button>
+    </div>
+    })}
+
+   
+   
 </div>
     </div>
 
